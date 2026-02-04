9f79c1b36a618e9cbcc5d050ffbed9e9569a50c7
Neil Berkman <neil@xuku.com>
2026-02-04 11:08:42 -0800

    Add precompiled NIF checksums for v0.5.0
    - Unignore checksum files so they're tracked in git
    - All 12 platform/NIF version combinations included

---
+++ checksum-Elixir.PDFRedlines.Native.exs
@@ -0,0 +1,14 @@
+%{
+  "libpdf_redlines_nif-v0.5.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:b8482687b25457f4a6caa51e511506db7feaf3a1e00c2f0aa9d261c2fd62cefc",
+  "libpdf_redlines_nif-v0.5.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:d717a8a4e5bc3eff6fe24c4f0bf342d58c1a0ca4d47535d3b9537e8d745c375e",
+  "libpdf_redlines_nif-v0.5.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:53f428d491b2f88a8d742db9c6a94610f0aa1103152636d4af8b8b234248a09c",
+  "libpdf_redlines_nif-v0.5.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:fb4accf40316125e2c383d01b119e16c20a98e61345f9c7202a6c88432c60cb7",
+  "libpdf_redlines_nif-v0.5.0-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:4587c1913a306bd817aa6a1c63c0bf0af16facb91b17d4aefad585619cd083f7",
+  "libpdf_redlines_nif-v0.5.0-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:874d6d89a771d5d775b2a616adf643098e75585db8a61cad841baf1a0d8fe911",
+  "libpdf_redlines_nif-v0.5.0-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:65ba06824835d6796eabcea0c70738ab90feea5d386c32b29bf8f1725e3eebb4",
+  "libpdf_redlines_nif-v0.5.0-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:1bd6409647af47d126269260596686b5f407de25f5956bba9e474fc5168b9987",
+  "libpdf_redlines_nif-v0.5.0-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:bf33c66a2f4f409d864f1da2004ccbbb400ad2806079474b9638e022d2e0608c",
+  "libpdf_redlines_nif-v0.5.0-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:7307b4f3eab26743a62862af5fd9782c7717dd1a7e07d50d56389081c7812ece",
+  "libpdf_redlines_nif-v0.5.0-nif-2.17-x86_64-apple-darwin.so.tar.gz" => "sha256:20c1ac48854b953f8b1aee60355741d7e4ced95165c09c9a811954c6813776ae",
+  "libpdf_redlines_nif-v0.5.0-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:b74603dcac57a4c7a9aec527f9aec754f004d268ccc8d4c1f1fc3954abd48f31",
+}
