529aebd408998aba9d513d66af5f23f495b70da3
Neil Berkman <neil@xuku.com>
2026-02-09 07:52:04 -0800

    Add v0.7.1 precompiled NIF checksums

--- checksum-Elixir.PDFRedlines.Native.exs
+++ checksum-Elixir.PDFRedlines.Native.exs
@@ -1,1 +1,14 @@
-%{}
+%{
+  "libpdf_redlines_nif-v0.7.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:00bb79a85d8d80aaa9d16cae25bc750d07733da51cf02347fde5f35184e5eeaa",
+  "libpdf_redlines_nif-v0.7.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:10fc0223c27ccf4c6158959b7aa83a90e381ce375f9848284fc92aba3d8552fe",
+  "libpdf_redlines_nif-v0.7.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:945c847944691d957ce77817bf29decb6c277b304a20dd0510b1634023746199",
+  "libpdf_redlines_nif-v0.7.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:08f9320608a70c1b42446f1cb9994d26958ecb80c34e1f7107a494c9b7029f3a",
+  "libpdf_redlines_nif-v0.7.1-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:cc4512cedbbfb088aa244ef620cbd7a07f31956d4d676e5cbc8a316a9f70e017",
+  "libpdf_redlines_nif-v0.7.1-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:d936ac49b0105dd00e249a3fd3c727ced6bc0e9eba30564ae0eadb5792adbabe",
+  "libpdf_redlines_nif-v0.7.1-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:23311b3adcb96bbdf9cc544db35f404f8d93db75298f00a18da0a2990312618d",
+  "libpdf_redlines_nif-v0.7.1-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:51c27c663942ae751e4551ef45fa32c8a6d169fade55c4fec1cafb9f99ffc630",
+  "libpdf_redlines_nif-v0.7.1-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:ab4f8f0bd331246175588abc27fc650a8a1e5f62b4c9872f51ee6275c15b4fdc",
+  "libpdf_redlines_nif-v0.7.1-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:11e8b521ba14231c8b86e16f9b8555a75a0f7b52457f80e66521a534beb4fe3c",
+  "libpdf_redlines_nif-v0.7.1-nif-2.17-x86_64-apple-darwin.so.tar.gz" => "sha256:760418072e05e3c5687504e0d53698367c61b4a495c65d6bb9083f728375fd4c",
+  "libpdf_redlines_nif-v0.7.1-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:627f54f2248a76677820633dd374a0a8ca16a0ee35fb1d279bc5a3e86edb8b8e",
+}
